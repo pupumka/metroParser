8294dac23f6dc3660db7cbc5ea030242366658ba
Pumka <pupumka1@gmail.com>
2020-11-12 00:51:29 +0300

    comit

--- src/DataFormating.java
+++ src/DataFormating.java
@@ -1,29 +1,98 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class DataFormating {
+    public static Map<Integer,String> uniqueCategories = new HashMap<>();
+    public static ArrayList<Integer> uniqueParentId = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        String filePath = "C:\\Users\\mironov.matvey\\Documents\\GitHub\\metroParser\\src\\import\\1\\Состав.txt";
+        //String filePath = "C:\\Users\\mironov.matvey\\Documents\\GitHub\\metroParser\\src\\import\\1\\Категории.txt";
 
-        //String filePath = "E:\\Program Files\\metroParser\\src\\import\\1\\Общая информация.txt";
+        String filePath = "E:\\Program Files\\metroParser\\src\\import\\1\\Категории.txt";
         //String filePath = "C:\\Users\\mironov.matvey\\Documents\\GitHub\\metroParser\\src\\import\\1\\Общая информация.txt";
 
         ArrayList<String> list = readCommonInfo(filePath);
-        //list = deleteBrackets(list);
+        list = deleteBrackets(list);
+        categories1level(list);
+
+        filePath = "E:\\Program Files\\metroParser\\src\\import\\1\\Значения категорий.txt";
+        list = readCommonInfo(filePath);
+        list = deleteBrackets(list);
+        categories2(list);
+
         //printStructureFinal(list);
-        printSostav(list);
+        //printSostav(list);
 
         //printStructure(list);
 
         //for (String s : list){
         //    System.out.println(s);
         //}
+    }
+
+    public static void categories2(ArrayList<String> list){
+        for (int i =0; i<list.size();i++){
+            //System.out.println(list.get(i));
+            int listSize = list.get(i).split(";").length;
+            String categoryName = list.get(i).split(";")[listSize-1];
+            int counter = 0;
+            for (Map.Entry<Integer,String> entry : uniqueCategories.entrySet()){
+                if (entry.getValue().equals(categoryName)){
+                    System.out.println(uniqueParentId.get(counter)+","+entry.getKey());
+                }
+                counter++;
+            }
+        }
+    }
+
+    public static void categories1level(ArrayList<String> list){
+        //Map<Integer,String> uniqueCategories = new HashMap<>();
+        //ArrayList<Integer> uniqueCategoryId = new ArrayList<>();
+        //ArrayList<Integer> uniqueParentId = new ArrayList<>();
+
+        int categoryId = 100;
+        int parentId = 0;
+        for (int i =0; i< list.size();i++){
+            int categoryNum = list.get(i).split(";").length;
+            for (int j = 0;j<categoryNum;j++) {
+                if (j!=0){
+                    for (Map.Entry<Integer,String> entry : uniqueCategories.entrySet()){
+                        if (list.get(i).split(";")[j-1].equals(entry.getValue())){
+                            parentId = entry.getKey();
+                        }
+                    }
+                    //parentId = uniqueCategories.
+                }
+                if (j == 0) {
+                    parentId=0;
+                }
+                String categoryName = list.get(i).split(";")[j];
+
+                if (!uniqueCategories.containsValue(categoryName)) {
+                    uniqueCategories.put(categoryId, categoryName);
+                    uniqueParentId.add(parentId);
+                    categoryId++;
+                }
+
+                //System.out.println(categoryId+"@"+parentId+"@"+categoryName);
+
+                //categoryId++;
+            }
+        }
+
+        int z = 0;
+        //for (Map.Entry<Integer,String> entry : uniqueCategories.entrySet()){
+        //    System.out.println(entry.getKey()+"@"+uniqueParentId.get(z)+"@"+entry.getValue());
+        //    z++;
+       // }
+        //for (int k = 0;k<uniqueCategories.size();k++){
+        //    System.out.println(uniqueCategories.get(k)+"@"+uniqueParentId.get(k)+"@"+uniqueCategories.get(k));
+        //} //818 последняя строка по счету
+
+
     }
 
     public static void printSostav(ArrayList<String> list){
@@ -145,9 +214,16 @@ public class DataFormating {
     public static ArrayList<String> deleteBrackets(ArrayList<String> oldList){
         ArrayList<String> list = new ArrayList<String>();
         for (int i = 0; i<oldList.size();i++){
-            String s1 = oldList.get(i);
-            String s2 = s1.substring(1,s1.length()-1);
-            list.add(s2);
+
+            String s = oldList.get(i).replace("[","");
+            s = s.replace("]","");
+            if (i==0){
+                s = s.substring(1);
+            }
+            list.add(s);
+            //String s1 = oldList.get(i);
+            //String s2 = s1.substring(1,s1.length()-1);
+            //list.add(s2);
         }
         return list;
     }
